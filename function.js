5effa3a66df6754837d021a3414a196a3807ac5b
Prachi Shah <ps242215@gmail.com>
2021-03-24 23:12:30 +0530

    version1.1

---
+++ function.js
@@ -0,0 +1,34 @@
+function changeImage(song_number) {
+    switch (song_number) {
+        case 1:
+            document.getElementById("song-item 1").src = "Images/Song1.gif";
+            break;
+        case 2:
+            document.getElementById("song-item 2").src = "Images/Song2.gif";
+            break;
+        case 3:
+            document.getElementById("song-item 3").src = "Images/Song3.gif";
+            break;
+        case 4:
+            document.getElementById("song-item 4").src = "Images/Song4.gif";
+            break;
+        case 5:
+            document.getElementById("song-item 5").src = "Images/Song5.gif";
+            break;
+        case 6:
+            document.getElementById("song-item 6").src = "Images/Song6.gif";
+            break;
+        case 7:
+            document.getElementById("song-item 7").src = "Images/Song7.gif";
+            break;
+        case 8:
+            document.getElementById("song-item 8").src = "Images/Song8.gif";
+            break;
+        case 9:
+            document.getElementById("song-item 9").src = "Images/Song9.gif";
+            break;
+        case 10:
+            document.getElementById("song-item 10").src = "Images/Song10.gif";
+            break;
+    }
+}
